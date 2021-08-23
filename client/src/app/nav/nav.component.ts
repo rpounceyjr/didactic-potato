5c98a81c2335c06ea80e3f2ca4fccec4a515790c
Roger Pouncey <rogerpouncey@rogerponceysmbp.lan>
2021-08-23 10:39:28 -0400

    added authguard and routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
@@ -6,24 +8,33 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  model: any = {}
+  model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-    this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-    }, error => console.log(error))
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/members');
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.name);
+      }
+    );
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
-
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,14 +11,28 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   register() {
-    this.accountService.register(this.model).subscribe((response) => {
-      this.cancel();
-    }, error => console.log(error));
+    this.accountService.register(this.model).subscribe(
+      (response) => {
+        this.cancel();
+      },
+      (error) => {
+        console.log(error);
+        if(error.error.errors.Username) {
+          this.toastr.error(error.error.errors.Username)
+        }
+        if(error.error.errors.Password) {
+          this.toastr.error(error.error.errors?.Password)
+        }
+      }
+    );
   }
 
   cancel() {
